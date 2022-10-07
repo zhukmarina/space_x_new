a167a28c6a3a1d42d0cdf383813444e1e3b57f49
Marina <marinazhukova28@gmail.com>
2022-10-07 20:19:25 +0300

    bug fix

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import DetailsPage from "./pages/details/DetailsPage";
 import { Routes, Route ,} from "react-router-dom";
 import store from "./appStore";
 import './firebase';
-import Lllogin from './components/Login/Login'
-import SssignUp from "./components/SignUp/SignUp"
+import Login from './components/Login/Login'
+import SignUp from "./components/SignUp/SignUp"
 
 function App() {
   return (
@@ -18,11 +18,11 @@ function App() {
         <Header />
         <section className="itemContainer" >
         <Routes>
-        <Route exact path="/" element={<Home />} />
-          <Route exact path="/dragons" element={<Home />} />
+          <Route  path="/" element={<Home />} />
+          <Route  path="/dragons" element={<Home />} />
           <Route exact path="/dragons/:id" element={<DetailsPage />} />
-          <Route path="/signup" element={<SssignUp/>} />
-          <Route path="/login"  element={<Lllogin/>} />
+          <Route path="/signup" element={<SignUp/>} />
+          <Route path="/login"  element={<Login/>} />
           {/* <Route path="/signup" element={<SignUp/>} /> */}
           {/* <Route path="/login"  element={<Login/>} /> */}
         </Routes>
