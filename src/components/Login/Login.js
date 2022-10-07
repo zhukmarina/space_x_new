af2c20ce8f7db11d58d5f87af93fadb4a8850e0e
Marina <marinazhukova28@gmail.com>
2022-10-07 23:12:50 +0300

    add bootstrap

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,27 @@
 import { useDispatch } from "react-redux";
 import { useNavigate, Link } from "react-router-dom";
-
+import { Form, Button, Card, Alert, Container } from "react-bootstrap";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { Form } from "../Form";
+// import { Form } from "../Form";
 import { setUser } from "../../appStore/slices/userSlice";
+import { useState } from "react";
 
 const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
 
   const handleLogin = (email, password) => {
+    setError("");
+    setLoading(true);
     const auth = getAuth();
     signInWithEmailAndPassword(auth, email, password)
       .then(({ user }) => {
         console.log(user);
-        localStorage.setItem('authToken', user.email)
+        localStorage.setItem("authToken", user.email);
         dispatch(
           setUser({
             email: user.email,
@@ -23,18 +30,62 @@ const Login = () => {
           })
         );
         navigate("/");
-        
       })
-      .catch(() => alert("Invalid user!"));
+      .catch(() => setError("Failed to log in"));
   };
 
   return (
-  <div style={{display:"flex", flexDirection:"column",justifyContent:"center",alignItems:"center" }}>
-      <Form title=" log in " handleClick={handleLogin} />
-      <p>
-        Need an account? <Link style={{color:"blue"}}to="/signup">Sign Up</Link>
-      </p>
-    </div>
+    <>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Card border="info">
+            <Card.Body>
+              <h2 className="text-center mb-4">Log In</h2>
+              {error && <Alert variant="danger">{error}</Alert>}
+              <Form>
+                <Form.Group id="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    onChange={(event) => {
+                      setEmail(event.target.value);
+                    }}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group id="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    onChange={(event) => {
+                      setPass(event.target.value);
+                    }}
+                    required
+                  />
+                </Form.Group>
+                <Button
+                  onClick={() => handleLogin(email, pass)}
+                  disabled={loading}
+                  className="w-100"
+                  type="submit"
+                >
+                  Log In
+                </Button>
+              </Form>
+              <div className="w-100 text-center mt-2">
+                Need an account?{" "}
+                <Link style={{ color: "blue" }} to="/signup">
+                  Sign Up
+                </Link>
+              </div>
+            </Card.Body>
+          </Card>
+        </div>
+      </Container>
+    </>
   );
 };
 
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,14 +1,21 @@
 import { useDispatch } from "react-redux";
 import { useNavigate, Link } from "react-router-dom";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { Form } from "../Form";
 import { setUser } from "../../appStore/slices/userSlice";
+import { Form, Button, Card, Alert, Container } from "react-bootstrap";
+import { useState } from "react";
 
 const SignUp = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
 
   const handleRegister = (email, password) => {
+    setError("");
+    setLoading(true);
     const auth = getAuth();
     createUserWithEmailAndPassword(auth, email, password)
       .then(({ user }) => {
@@ -23,16 +30,58 @@ const SignUp = () => {
         );
         navigate("/");
       })
-      .catch(console.error);
+      .catch(() => setError("Failed to create an account"));
   };
 
   return (
-    <div style={{display:"flex", flexDirection:"column",justifyContent:"center",alignItems:"center" }}>
-      <Form title="register" handleClick={handleRegister} />
-      <p>
-        Already have an account? <Link style={{color:"blue"}} to="/login">Log In</Link>
-      </p>
-    </div>
+    <>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Card border="info">
+            <Card.Body>
+              <h2 className="text-center mb-4">Sign Up</h2>
+              {error && <Alert variant="danger">{error}</Alert>}
+              <Form>
+                <Form.Group id="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    onChange={(event) => {
+                      setEmail(event.target.value);
+                    }}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group id="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    onChange={(event) => {
+                      setPass(event.target.value);
+                    }}
+                    required
+                  />
+                </Form.Group>
+                <Button
+                  onClick={() => handleRegister(email, pass)}
+                  disabled={loading}
+                  className="w-100"
+                  type="submit"
+                >
+                  Sign Up
+                </Button>
+              </Form>
+              <div className="w-100 text-center mt-2">
+            Already have an account? <Link style={{ color: "blue" }} to="/login">Login</Link>
+          </div>
+            </Card.Body>
+          </Card>
+        </div>
+      </Container>
+    </>
   );
 };
 
